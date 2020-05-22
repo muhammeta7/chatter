23429b86a82531849348755c63bf1eefe6a9a37d
muhammeta7 <muhammeta7@gmail.com>
2020-05-22 16:46:31 -0400

    added error message for login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,35 +1,34 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthenticationService} from '../shared/authentication.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../shared/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   username = '';
   password = '';
   invalidLogin = false;
 
-  constructor(private router: Router, private loginService: AuthenticationService) {}
+  constructor(
+    private router: Router,
+    private loginService: AuthenticationService
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {}
 
-  }
-
-  checkLogin(){
-    (this.loginService.authenticate(this.username, this.password).subscribe(
-        data => {
-          this.router.navigate(['/dashboard']);
-          this.invalidLogin = false;
-        },
-        error => {
-          this.invalidLogin = true;
-
-        }
-      )
+  checkLogin() {
+    this.loginService.authenticate(this.username, this.password).subscribe(
+      (data) => {
+        this.router.navigate(['/dashboard']);
+        this.invalidLogin = false;
+      },
+      (error) => {
+        this.invalidLogin = true;
+        alert('Invalid credentials');
+      }
     );
   }
 }
